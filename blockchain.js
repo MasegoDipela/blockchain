ac9f283c0a0848cce975dd56cdd79fd4d6e6ffc7
Masego Dipela <masegolxve@gmail.com>
2026-02-01 22:47:09 +0200

    Add blockchain functionality with initial setup and validation

---
+++ blockchain.js
@@ -0,0 +1,45 @@
+[
+  {
+    "hash": "0",
+    "previousHash": null
+  },
+  {
+    "hash": "0.6645526534047352",
+    "previousHash": "0"
+  },
+  {
+    "hash": "0.380449867983663",
+    "previousHash": "0.6645526534047352"
+  },
+  {
+    "hash": "0.491939498851252",
+    "previousHash": "0.380449867983663"
+  },
+  {
+    "hash": "0.9601207688214994",
+    "previousHash": "0.491939498851252",
+    "data": {
+      "fromAddress": "Me",
+      "toAddress": "You",
+      "amount": 20
+    }
+  },
+  {
+    "hash": "0.09631974588994785",
+    "previousHash": "0.9601207688214994",
+    "data": {
+      "fromAddress": "Me",
+      "toAddress": "You",
+      "amount": 20
+    }
+  },
+  {
+    "hash": "0.28958209480141917",
+    "previousHash": "0.09631974588994785",
+    "data": {
+      "fromAddress": "Me",
+      "toAddress": "You",
+      "amount": 30
+    }
+  }
+]
