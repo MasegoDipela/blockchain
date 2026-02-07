282516125a8190b0acc92d511a53256e4708739d
Masego Dipela <masegolxve@gmail.com>
2026-02-08 01:34:29 +0200

    Add transaction management with write and get functions, and initial transactions data

---
+++ transactions.js
@@ -0,0 +1,17 @@
+[
+  {
+    "fromAddress": "You",
+    "toAddress": "Me",
+    "amount": 15
+  },
+  {
+    "fromAddress": "You",
+    "toAddress": "Me",
+    "amount": 25
+  },
+  {
+    "fromAddress": "You",
+    "toAddress": "Me",
+    "amount": 35
+  }
+]
